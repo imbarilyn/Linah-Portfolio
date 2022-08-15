fd371de2a89a3b832d131ac64f3da54bb3fba1dc
Linah Muhonja <linahmuhonjaimbari@gmail.com>
2022-08-15 08:32:10 +0300

    added a button, event listener and the class name on javascript file

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const h2 = document.createElement('h2');
 h2.textContent = "This content added by Javascript";
 document.querySelector("body").appendChild(h2);
+const button = document.createElement('button');
+button.textContent = "click me";
+document.querySelector('body').append(button);
+button.className = "btn-container";
+const clickMe = document.querySelector('button');
+clickMe.addEventListener('click', function(){
+    alert(`Clicked already`);
+});
+
