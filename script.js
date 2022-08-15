8d19cce089d03200aa2ac70f18d20d809211a27b
Linah Muhonja <linahmuhonjaimbari@gmail.com>
2022-08-15 08:50:41 +0300

    Added a list on the javascript

--- script.js
+++ script.js
@@ -9,4 +9,7 @@ const clickMe = document.querySelector('button');
 clickMe.addEventListener('click', function(){
     alert(`Clicked already`);
 });
+const div =  document.createElement('div');
+div.innerHTML = "<ul><li>Billy Black</li><li>Brandy</li><li>Bensoul</li></ul>"
+document.querySelector('body').append(div);
 
